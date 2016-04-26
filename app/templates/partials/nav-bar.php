6d674bc7f63786ee9c301faca97624e055cd9459
Ivan Dev <g.ivan@dizboard.com>
2016-04-26 17:35:55 +0300

    123qwe

--- app/templates/partials/nav-bar.php
+++ app/templates/partials/nav-bar.php
@@ -1,9 +1,12 @@
+<?php
+/** @var \Dez\Authorizer\Adapter\Session $authorizerSession */
+?>
 <div class="row">
     <div class="grid-10">
         <a class="button button-success" href="<?= $url->path('manager/upload-file'); ?>">+ upload file</a>
         <a class="button button-notice" href="<?= $url->path('manager/categories'); ?>">categories</a>
         <a class="button button-warning" href="<?= $url->path('manager/files/latest'); ?>">latest</a>
         <a class="button button-danger pull-right" href="<?= $url->path('manager/close-session'); ?>">logout</a>
-        <a class="button button-pink pull-right" href="<?= $url->path('manager/users/profile'); ?>">profile</a>
+        <a class="button button-pink pull-right" href="<?= $url->path('manager/users/profile'); ?>"><?= $authorizerSession->credentials()->getEmail(); ?></a>
     </div>
 </div>
